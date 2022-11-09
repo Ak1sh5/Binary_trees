e35ea1308ad4157aa7b75b85f3850481fb0ac843
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2022-11-10 00:23:22 +0530

    Update 9.Max_path_sum.cpp

--- 9.Max_path_sum.cpp
+++ 9.Max_path_sum.cpp
@@ -1,2 +1,18 @@
-//TC:
-//SC:
+//TC:O(n)
+//SC:O(n)
+class Solution {
+    int f(TreeNode*root,int &maxi)
+    {
+        if(root==NULL) return 0;
+        int l=max(0,f(root->left,maxi));
+        int r=max(0,f(root->right,maxi));
+        maxi=max(maxi,root->val+l+r);
+        return max(l,r)+root->val;
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxi=INT_MIN;
+        f(root,maxi);
+        return maxi;
+    }
+};

21de728c745d0bb4881e9d60c0ac9212e3140d93
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2022-12-06 19:04:54 +0530

    Create 12.Zig_Zag.cpp

---
+++ 12.Zig_Zag.cpp
@@ -0,0 +1,28 @@
+//TC: O(n) SC: O(n) in worst case of a skewed tree
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool left_to_right=true;
+
+        while(!q.empty()){
+            int size=q.size();
+            vector<int>ds(size);
+            for(int i=0;i<size;i++){
+                root=q.front();
+                q.pop();
+                int index = left_to_right?i:(size-i-1);
+                ds[index]=root->val;
+                if(root->left)q.push(root->left);
+                if(root->right)q.push(root->right);
+            }
+            left_to_right=!left_to_right;
+                ans.push_back(ds);
+       
+        }
+        return ans;
+    }
+};
